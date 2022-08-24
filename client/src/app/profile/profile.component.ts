725a6b577b3f99447be8efc2fbc7b57aa699fda0
Tenzin Mahabir <tmahabir10@gmail.com>
2022-08-23 22:44:24 -0700

    User's profile only shows their images, also updated styling

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { AuthenticationService, UserDetails } from "../authentication.service";
+import { ImageService } from "../image.service";
+import { Image } from '../models/Image';
 
 @Component({
   templateUrl: "./profile.component.html",
@@ -7,8 +10,9 @@ import { AuthenticationService, UserDetails } from "../authentication.service";
 })
 export class ProfileComponent implements OnInit {
   details: UserDetails;
-
-  constructor(private auth: AuthenticationService) {}
+  images: Image[] = [];
+  private imageSubscription: Subscription;
+  constructor(private auth: AuthenticationService, private imageService: ImageService) {}
 
   ngOnInit() {
     this.auth.profile().subscribe(
@@ -19,5 +23,14 @@ export class ProfileComponent implements OnInit {
         console.error(err);
       }
     );
+
+    this.imageService.getImages();
+    this.imageSubscription = this.imageService.getImagesStream().subscribe((images: Image[]) => {
+      this.images = images;
+    });
+  }
+
+  onDelete(filePath: string){
+    this.imageService.deleteImage(filePath);
   }
 }
